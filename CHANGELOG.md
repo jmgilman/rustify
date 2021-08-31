e282b11ff4e0fb8729192b0aa65f6fe7bd461083
Joshua Gilman <joshuagilman@gmail.com>
2021-08-30 21:34:34 -0700

    Adds support for additional HTTP methods

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+- Additional HTTP request methods
+
 ## [0.4.1] - 2021-08-26
 
 ### Added
--- src/enums.rs
+++ src/enums.rs
@@ -3,22 +3,32 @@
 /// Represents a HTTP request method
 #[derive(Clone, Debug)]
 pub enum RequestMethod {
+    CONNECT,
     DELETE,
     GET,
     HEAD,
     LIST,
+    OPTIONS,
+    PATCH,
     POST,
+    PUT,
+    TRACE,
 }
 
 #[allow(clippy::from_over_into)]
 impl Into<http::Method> for RequestMethod {
     fn into(self) -> http::Method {
         match self {
+            RequestMethod::CONNECT => http::Method::CONNECT,
             RequestMethod::DELETE => http::Method::DELETE,
             RequestMethod::GET => http::Method::GET,
             RequestMethod::HEAD => http::Method::HEAD,
             RequestMethod::LIST => http::Method::from_bytes("LIST".as_bytes()).unwrap(),
+            RequestMethod::OPTIONS => http::Method::OPTIONS,
+            RequestMethod::PATCH => http::Method::PATCH,
             RequestMethod::POST => http::Method::POST,
+            RequestMethod::PUT => http::Method::PUT,
+            RequestMethod::TRACE => http::Method::TRACE,
         }
     }
 }
