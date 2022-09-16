e43ed7cdab0a86929e5652731e9bf6ba6c00b3ab
Kornel <kornel@geekhood.net>
2022-09-16 13:30:37 +0100

    Inline tiny module files

--- src/blocking.rs
+++
@@ -1,5 +0,0 @@
-//! Contains blocking variants of clients for executing
-//! [Endpoints][crate::endpoint::Endpoint]
-
-pub mod client;
-pub mod clients;
--- src/blocking/clients.rs
+++
@@ -1,4 +0,0 @@
-//! Contains implementations of [Client][crate::blocking::client::Client] which
-//! use varying blocking HTTP clients.
-#[cfg(feature = "reqwest")]
-pub mod reqwest;
