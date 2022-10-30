b19e39d2b5173750599f18fe32917403f661ee07
George Miao <gm@miao.dev>
2022-10-30 06:40:35 -0400

    feat: better error message

--- src/errors.rs
+++ src/errors.rs
@@ -6,44 +6,44 @@ use crate::enums::RequestMethod;
 /// The general error type returned by this crate
 #[derive(Error, Debug)]
 pub enum ClientError {
-    #[error("Error parsing endpoint into data")]
+    #[error("Error parsing endpoint into data: {source}")]
     DataParseError { source: anyhow::Error },
-    #[error("Error building endpoint request")]
+    #[error("Error building endpoint request: {source}")]
     EndpointBuildError { source: anyhow::Error },
-    #[error("An error occurred in processing the request")]
+    #[error("An error occurred in processing the request: {source}")]
     GenericError { source: anyhow::Error },
-    #[error("Error sending HTTP request")]
+    #[error("Error sending HTTP request: {source}")]
     RequestError {
         source: anyhow::Error,
         url: String,
         method: String,
     },
-    #[error("Error building HTTP request")]
+    #[error("Error building HTTP request: {source}")]
     RequestBuildError {
         source: http::Error,
         method: RequestMethod,
         url: String,
     },
-    #[error("Error building request for Reqwest crate")]
+    #[error("Error building request for Reqwest crate: {source}")]
     ReqwestBuildError { source: reqwest::Error },
-    #[error("Error retrieving HTTP response")]
+    #[error("Error retrieving HTTP response: {source}")]
     ResponseError { source: anyhow::Error },
-    #[error("Error parsing server response as UTF-8")]
+    #[error("Error parsing server response as UTF-8: {source}")]
     ResponseConversionError {
         source: anyhow::Error,
         content: Vec<u8>,
     },
-    #[error("Error parsing HTTP response")]
+    #[error("Error parsing HTTP response: {source}")]
     ResponseParseError {
         source: anyhow::Error,
         content: Option<String>,
     },
-    #[error("Server returned error")]
+    #[error("Server returned error (HTTP {code})")]
     ServerResponseError { code: u16, content: Option<String> },
-    #[error("Error building URL")]
+    #[error("Error building URL: {source}")]
     UrlBuildError { source: http::uri::InvalidUri },
-    #[error("Error serializing URL query parameters")]
+    #[error("Error serializing URL query parameters: {source}")]
     UrlQueryParseError { source: anyhow::Error },
-    #[error("Error parsing URL")]
+    #[error("Error parsing URL: {source}")]
     UrlParseError { source: url::ParseError },
 }
